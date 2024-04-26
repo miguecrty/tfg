953937331cfdeb063e64fd4bccb830c2e9d3437a
Tu Nombre <you@example.com>
2024-04-26 19:45:48 +0200

    install k8s

---
+++ ficheros_instalacion/instalacion_kubernetes.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo "-----------------------------------------------------------";
+echo "-----------------------------------------------------------";
+echo "-----------------------------------------------------------";
+echo "---------  Bienvenido al instalador de Kubernetes  --------";
+echo "-----------------------------------------------------------";
+echo "-----------------------------------------------------------";
+echo "-----------------------------------------------------------";
+echo "¿Desea continuar? (y/n)"
+read response
+if [ "$response" = "y" ]; then
+echo "Instalando kind...";
+sleep 1    
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.22.0/kind-$(uname)-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+echo "Instalando kubectl...";
+sleep 1
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm -rf kubectl
+echo "---------  Kubernetes instalado correctamente  --------";
+else
+    echo "Kubernetes no se instalará"
+fi
+
+
+    
