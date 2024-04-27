b65e39d8fd5909eea27d7081311cd3372c9762b1
Tu Nombre <you@example.com>
2024-04-27 19:16:15 +0200

    instalacion k8s

--- ficheros_instalacion/instalacion_kubernetes.sh
+++ ficheros_instalacion/instalacion_kubernetes.sh
@@ -20,6 +20,16 @@ sleep 1
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 rm -rf kubectl
+echo "Instalando gcloud...";
+sleep 1
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-472.0.0-linux-x86_64.tar.gz
+tar -xf google-cloud-cli-472.0.0-linux-x86_64.tar.gz
+./google-cloud-sdk/install.sh
+source ~/.bashrc
+echo "Instalando componentes de gcloud...";
+sleep 1
+gcloud components update
+gcloud components install gke-gcloud-auth-plugin
 echo "---------  Kubernetes instalado correctamente  --------";
 else
     echo "Kubernetes no se instalará"
